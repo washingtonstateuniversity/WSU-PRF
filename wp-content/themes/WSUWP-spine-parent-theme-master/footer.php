64c0fd2bf7ff3fed6287994efd23d142e68dd8d2
Jeremy Bass <jeremybass@cableone.net>
2015-01-20 11:34:10 -0800

    add the spine theme in the mix for policies

---
+++ wp-content/themes/WSUWP-spine-parent-theme-master/footer.php
@@ -0,0 +1,14 @@
+<?php get_template_part( 'spine' ); ?>
+
+<?php get_template_part('parts/after-main'); ?>
+</div><!--/binder-->
+<?php get_template_part('parts/after-binder'); ?>
+</div><!--/jacket-->
+<?php get_template_part('parts/after-jacket'); ?>
+
+<?php get_template_part('parts/contact'); ?>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wp-content/themes/WSUWP-spine-parent-theme-master/spine/header.php
@@ -0,0 +1,14 @@
+<header class="spine-header">
+	<a href="<?php echo spine_get_campus_home_url(); ?>" id="wsu-signature">Washington State University</a>
+</header>
+
+<section id="wsu-actions" class="spine-actions clearfix">
+
+	<ul id="wsu-actions-tabs" class="spine-actions-tabs spine-tabs clearfix">
+		<li id="wsu-search-tab" class="spine-search-tab closed"><button>Search</button></li>
+		<li id="wsu-contact-tab" class="spine-contact-tab closed"><button>Contact</button></li>
+		<li id="wsu-share-tab" class="spine-share-tab closed"><button>Share</button></li>
+		<li id="wsu-print-tab" class="spine-print-tab closed"><button>Print</button></li>
+	</ul>
+
+</section><!--/#wsu-actions-->
